adbdcb8893b080801277e975c7ea41d38ec213a4
Yadunund <yadunund@intrinsic.ai>
2024-06-15 05:48:50 +0800

    Rebase from 'debian/jazzy/rmf_visualization_building_systems'

--- rmf_visualization_building_systems/rmf_visualization_building_systems.py
+++ rmf_visualization_building_systems/rmf_visualization_building_systems.py
@@ -1,3 +1,17 @@
+# Copyright 2024 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import rclpy
 from rclpy.node import Node
 
@@ -35,10 +49,10 @@ class BuildingSystemsVisualizer(Node):
         self.get_logger().info('Building systems visualizer started...')
 
         qos = QoSProfile(
-            history=History.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+            history=History.KEEP_LAST,
             depth=1,
-            reliability=Reliability.RMW_QOS_POLICY_RELIABILITY_RELIABLE,
-            durability=Durability.RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL)
+            reliability=Reliability.RELIABLE,
+            durability=Durability.TRANSIENT_LOCAL)
 
         self.marker_pub = self.create_publisher(
             MarkerArray,
